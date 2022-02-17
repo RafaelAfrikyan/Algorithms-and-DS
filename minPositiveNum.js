0919e409c408a73ab54e75f731e1f45fb06fb698
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2022-02-17 15:32:41 +0400

    feat resolving minPositive number

--- minPositiveNum.js
+++ minPositiveNum.js
@@ -1,31 +1,25 @@
-
-
 //2. Given an array of numbers. Write a recursive function to find its minimal positive
 //element. (if such element does not exist, return -1).
 
-let arr = [56, -9, 87, -23, 0, -105, 55, 1];
+//another way of resolving with recursion
+
+let arr = [-9, -23, -105];
 
 function minPositive(arr) {
   if (arr.length === 1) {
+    if (arr[0] < 0) {
+      return -1;
+    }
     return arr[0];
   }
-
   let firstNum = arr[0];
   let lastNum = arr[arr.length - 1];
 
-  if (firstNum < 0) {
+  if (firstNum < 0 || (firstNum >= lastNum && lastNum >= 0)) {
     arr.shift();
-  }
-  if (lastNum < 0) {
+  } else if (lastNum < 0 || (lastNum >= firstNum && firstNum >= 0)) {
     arr.pop();
   }
-
-  if (firstNum > lastNum) {
-    arr.shift();
-  } else {
-    arr.pop();
-  }
-
   return minPositive(arr);
 }
 
